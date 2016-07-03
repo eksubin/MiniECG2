33cd912ebab10306dca90226e0489510ba984109
eksubin <subinedu@gmail.com>
2016-07-03 14:38:17 +0530

    repo details

--- app/src/main/java/ecg/imaginatio/subin/miniecg/ecgstart.java
+++ app/src/main/java/ecg/imaginatio/subin/miniecg/ecgstart.java
@@ -3,14 +3,13 @@ package ecg.imaginatio.subin.miniecg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ecgstart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ecgstart);
+        // renuka should add the recycler views to this activity
 
-        //keep this activity blank
-        //this activity will be used to create the first loading page
     }
 }
--- app/src/main/java/ecg/imaginatio/subin/miniecg/page1.java
+++ app/src/main/java/ecg/imaginatio/subin/miniecg/page1.java
@@ -3,14 +3,11 @@ package ecg.imaginatio.subin.miniecg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class page1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //keep this activity blank
-        //this activity will be used to create the first loading page
+        setContentView(R.layout.activity_page1);
     }
 }
